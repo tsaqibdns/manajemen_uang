2132ee422bd7f28e8ed9818bd47c3ba76db30a7e
Tsaqib Abdan <tsaqibabdanes@gmail.com>
2025-01-01 21:20:42 +0700

    update baru

--- script-login.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('login-form').addEventListener('submit', function(event) {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
-    if (!username || !password) {
-        alert('Username dan Password harus diisi!');
-        event.preventDefault();
-    }
-});
